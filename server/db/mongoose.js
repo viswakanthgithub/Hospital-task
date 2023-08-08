cd045b58a143c4372a87ba22b168b724150c5493
viswakanthgithub <125343605+viswakanthgithub@users.noreply.github.com>
2023-08-08 14:29:58 +0530

    Update mongoose.js

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -7,3 +7,4 @@ mongoose.Promise = global.Promise;
 mongoose.connect("mongodb+srv://viswakanth17:Hnz6lnpwMXSvH1jk@cluster0.fzsgpw7.mongodb.net/?retryWrites=true&w=majority");
 
 module.exports = {mongoose};
+
