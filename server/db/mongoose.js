3492024e08ca4f6d053df3abc155e8f3362f649f
viswakanthgithub <125343605+viswakanthgithub@users.noreply.github.com>
2023-08-08 14:27:18 +0530

    Update mongoose.js

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,15 +1,9 @@
-var mongoose = require('mongoose');
+var mongoose = require ('mongoose');
+
 
 mongoose.Promise = global.Promise;
 
-// Change the database connection URL with yours
-mongoose.connect("mongodb+srv://viswakanth17:Hnz6lnpwMXSvH1jk@cluster0.fzsgpw7.mongodb.net/?retryWrites=true&w=majority", {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-}).then(() => {
-  console.log('Connected to MongoDB successfully!');
-}).catch((error) => {
-  console.error('Error connecting to MongoDB:', error.message);
-});
+//change the database with yours
+mongoose.connect("mongodb+srv://viswakanth17:Hnz6lnpwMXSvH1jk@cluster0.fzsgpw7.mongodb.net/?retryWrites=true&w=majority");
 
-module.exports = { mongoose };
+module.exports = {mongoose};
