9f50b04d00dbdf4248f6d83ed4ee92c5bc8deb53
viswakanthgithub <125343605+viswakanthgithub@users.noreply.github.com>
2023-08-08 14:15:14 +0530

    Update mongoose.js

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -3,7 +3,7 @@ var mongoose = require('mongoose');
 mongoose.Promise = global.Promise;
 
 // Change the database connection URL with yours
-mongoose.connect("mongodb+srv://viswakanth17:R5U2ZzHqba4BHBXM@cluster0.wmslxde.mongodb.net/?retryWrites=true&w=majority", {
+mongoose.connect("mongodb+srv://viswakanth17:Hnz6lnpwMXSvH1jk@cluster0.fzsgpw7.mongodb.net/?retryWrites=true&w=majority", {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 }).then(() => {
